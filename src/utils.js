6d3a73f3b313dfb1b01b7445884073525eadbf25
dimka-zvd <ddavydkinD@gmail.com>
2025-06-06 12:47:06 +0300

    fix

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
 export default (min = 0, max = 100) => { // Рандомайзер
-  return Math.floor(Math.random() * (max - min + 1))+ min
+  const randomNum = Math.floor(Math.random() * (max - min + 1))+ min
+  return randomNum
 }
