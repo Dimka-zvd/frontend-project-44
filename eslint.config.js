b67d9733d7ff4a6ac19d2dc9e85e1cbc0270081f
dimka-zvd <ddavydkinD@gmail.com>
2025-11-20 10:44:47 +0300

    tests1

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,8 @@
-import js from '@eslint/js'
 import globals from 'globals'
-import { defineConfig } from 'eslint/config'
-import stylistic from '@stylistic/eslint-plugin'
+import pluginJs from '@eslint/js'
 
-export default defineConfig([
-  {
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    rules: {
-      '@stylistic/indent': ['error', 2],
-      '@stylistic/quotes': ['error', 'single'],
-      '@stylistic/semi': ['error', 'never'],
-      '@stylistic/comma-dangle': ['error', 'always-multiline'],
-      'no-console': 'off',
-      'import/extensions': 'off',
-    },
-  },
-  { files: ['/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
-])
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+]
